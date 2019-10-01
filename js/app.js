9d8aab37d70fab0f5716e0184a5163ef0b66d7fa
KennyK-TW <b2khakashu@yahoo.com>
2019-10-01 15:51:31 -0700

    adding more, still formating

--- js/app.js
+++ js/app.js
@@ -1,1 +1,98 @@
-console.log('Play Game');
+$("button").on("click", () => {
+    console.log("Game Start");
+    // createSquares(200); // for testing prior to setting up the rounds
+    setTimer();
+    setUpRound();
+});
+
+const createSquares = numberOfSquares => {
+    const $squares = $(".squares");
+    for (let i = 0; i < numberOfSquares; i++) {
+      const $square = $("<div/>");
+      $square.addClass("square");
+      $square.css("background-color", applyRandomColor());
+      $squares.append($square);
+    }
+  };
+
+  const applyRandomColor = () => {
+    const colors = ["red", "green", "blue"];
+    const index = Math.floor(Math.random() * colors.length);
+    return colors[index];
+  };
+
+  $(".squares").on("click",".square", e => {
+    handlePoke(e);
+  });
+  
+  
+let score = 0;
+
+const handlePoke = e => {
+  if (e.target.classList.contains("poked")) {
+    return;
+  }
+  if (e.target.classList.contains("square")) {
+    $(e.target).css("opacity", 0.3);
+    const color = $(e.target).css("background-color");
+    $(e.target).addClass("poked");
+    checkValidPoke(color);
+  }
+};
+
+const checkValidPoke = squareColor => {
+  // console.log('SQUARE', square, typeof square);
+  const colors = squareColor.substring(4, squareColor.length - 1).split(" "); // Get rgba from div inline style
+  const blue = parseInt(colors[2]);
+  if (blue === 255) {
+    score++;
+    $("h1").text("scoreboard: " + score);
+  } else {
+    score--;
+    $("h1").text("scoreboard: " + score);
+  }
+};
+
+
+let time = 30;
+let round = 1;
+
+const setTimer = () => {
+  const timer = setInterval(() => {
+    time--;
+    if (time === 0) {
+      round++;
+      clearInterval(timer);
+      setUpRound();
+      setTimer();
+    }
+    updateTime();
+  }, 1000);
+};
+
+const updateTime = () => {
+  const $timer = $("#timer");
+  $timer.text(`timer: ${time}s`);
+};
+
+
+
+const setUpRound = () => {
+    $(".squares").empty();
+    $("#round").text("round: " + round);
+  
+    if (round === 1) {
+      createSquares(50);
+      time = 30;
+    } else if (round === 2) {
+      createSquares(100);
+      time = 20;
+    } else if (round === 3) {
+      createSquares(150);
+      time = 10;
+    } else {
+      createSquares(250);
+      time = 10;
+    }
+  };
+  
